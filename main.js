8b1f0c934a3f9eb2b06a690afa2c19ff80aa3524
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2023-04-03 19:17:05 +0800

    Add content and structures styling

---
+++ main.js
@@ -0,0 +1,94 @@
+const genreField = document.querySelector("div#genre-cont fieldset");
+const genreGrid = document.querySelector('div#genre-grid');
+
+const selectMonthButton = document.querySelector('button#get-month');
+    selectMonthButton.addEventListener('click', popMonthSelect);
+const monthSelector = document.querySelector('dialog#month-selector');
+const months = document.querySelectorAll('button.month');
+    months.forEach(month => {month.addEventListener('click', selectMonth)});
+
+// Adds genre (start) - 
+const genreArray = [];
+function Genre(name, labelText ) {
+    this.name = name;
+    this.labelText = labelText;
+    genreArray.push(this);
+}
+
+const literaryFiction = new Genre('literary-fiction', 'Literary Fiction');
+const mystery = new Genre('mystery', 'Mystery');
+const thriller = new Genre('thriller', 'Thriller');
+const horror = new Genre('horror', 'Horror');
+const historical = new Genre('historical', 'Historical');
+const romance = new Genre('romance', 'Romance');
+const western = new Genre('western', 'Western');
+const bildungsroman = new Genre('bildungsroman', 'Bildungsroman');
+const speculativeFiction = new Genre('speculative-fiction', 'Speculative Fiction');
+const scienceFiction = new Genre('science-fiction', 'Science Fiction');
+const fantasy = new Genre('fantasy', 'Fantasy');
+const dystopian = new Genre('dystopian', 'Dystopian');
+const magicalRealism = new Genre('magical-realism', 'Magical Realism');
+const realistLiterature = new Genre('realist-literature', 'Realist Literature');
+
+// Sorts the genre alphabetically (start) --
+genreArray.sort((a, b) => {
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toUpperCase();
+
+    if (nameA < nameB) {
+        return -1;
+      }
+    
+    if (nameA > nameB) {
+        return 1;
+      }
+      
+    return 0;
+});
+// Sorts the genre alphabetically (end) --
+
+function addGenre(genre) {
+    const genreContainer = document.createElement('div');
+    genreContainer.setAttribute('class', 'checkbox-cont');
+
+    const genreCheckbox = document.createElement('input');
+    genreCheckbox.setAttribute('type', 'checkbox');
+    genreCheckbox.setAttribute('id', `${genre.name}`);
+    genreCheckbox.setAttribute('value', `${genre.name}`);
+
+    const genreLabel = document.createElement('label');
+    genreLabel.setAttribute('for', `${genre.name}`);
+    genreLabel.textContent = `${genre.labelText}`;
+
+    genreContainer.appendChild(genreCheckbox);
+    genreContainer.appendChild(genreLabel);
+
+    genreGrid.appendChild(genreContainer);
+}
+
+genreArray.forEach(genre => addGenre(genre));
+// Adds genre (end) - 
+
+
+// Pops Month Selector
+
+function popMonthSelect() {
+    monthSelector.showModal();
+}
+
+function selectMonth() {
+    const monthValue = this.getAttribute('value');
+    selectMonthButton.setAttribute('value', monthValue);
+    selectMonthButton.setAttribute('class', '');
+
+    if (monthValue === '') {
+        selectMonthButton.textContent = 'Select Month';
+        selectMonthButton.setAttribute('class', 'empty');
+    } else {
+        selectMonthButton.textContent = monthValue;
+    }
+
+    monthSelector.close();
+}
+
+
